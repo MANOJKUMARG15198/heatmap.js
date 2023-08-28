9771f4ff569c1ac1d14f776546c8ed7e160025cc
Manoj <manoj@zancompute.com>
2023-08-28 16:47:58 +0530

    InitailBuild

--- build/heatmap.js
+++ build/heatmap.js
@@ -54,6 +54,7 @@ var Store = (function StoreClosure() {
   Store.prototype = {
     // when forceRender = false -> called from setData, omits renderall event
     _organiseData: function(dataPoint, forceRender) {
+      console.log("_organiseData", dataPoint)
         var x = dataPoint[this._xField];
         var y = dataPoint[this._yField];
         var radi = this._radi;
